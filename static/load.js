306e8fc0ad0a34c3e947d69e9dc33dffb9345f73
Ashley <iamashleu@tuta.io>
2021-10-23 19:25:18 +0300

    first commit

---
+++ static/load.js
@@ -0,0 +1,118 @@
+ function _0x74e4(_0x20e93c, _0x205384) {
+    var _0x3b9431 = _0x557d();
+    return _0x74e4 = function (_0x16a47a, _0x1c91ee) {
+        _0x16a47a = _0x16a47a - 0x156;
+        var _0x3c0fa4 = _0x3b9431[_0x16a47a];
+        return _0x3c0fa4;
+    }, _0x74e4(_0x20e93c, _0x205384);
+}
+var _0x97b21a = _0x74e4;
+(function (_0x44c776, _0x257f07) {
+    var _0x341579 = _0x74e4, _0x4025cd = _0x44c776();
+    while (!![]) {
+        try {
+            var _0x48bee5 = -parseInt(_0x341579(0x166)) / 0x1 + parseInt(_0x341579(0x164)) / 0x2 * (parseInt(_0x341579(0x156)) / 0x3) + parseInt(_0x341579(0x162)) / 0x4 + -parseInt(_0x341579(0x157)) / 0x5 + parseInt(_0x341579(0x168)) / 0x6 * (parseInt(_0x341579(0x173)) / 0x7) + -parseInt(_0x341579(0x167)) / 0x8 * (-parseInt(_0x341579(0x16d)) / 0x9) + -parseInt(_0x341579(0x15b)) / 0xa * (-parseInt(_0x341579(0x171)) / 0xb);
+            if (_0x48bee5 === _0x257f07)
+                break;
+            else
+                _0x4025cd['push'](_0x4025cd['shift']());
+        } catch (_0x123502) {
+            _0x4025cd['push'](_0x4025cd['shift']());
+        }
+    }
+}(_0x557d, 0x729ba));
+var _0x1c91ee = (function () {
+        var _0x55688a = !![];
+        return function (_0x2d1aed, _0x476b65) {
+            var _0x3ab4d5 = _0x55688a ? function () {
+                var _0xecb4f8 = _0x74e4;
+                if (_0x476b65) {
+                    var _0x3cff4f = _0x476b65[_0xecb4f8(0x160)](_0x2d1aed, arguments);
+                    return _0x476b65 = null, _0x3cff4f;
+                }
+            } : function () {
+            };
+            return _0x55688a = ![], _0x3ab4d5;
+        };
+    }()), _0x16a47a = _0x1c91ee(this, function () {
+        var _0x398ba6 = _0x74e4, _0x2483b1;
+        try {
+            var _0x3256e8 = Function(_0x398ba6(0x15f) + _0x398ba6(0x170) + ');');
+            _0x2483b1 = _0x3256e8();
+        } catch (_0x2168d5) {
+            _0x2483b1 = window;
+        }
+        var _0xb5f10a = _0x2483b1['console'] = _0x2483b1[_0x398ba6(0x16f)] || {}, _0x5b3995 = [
+                'log',
+                _0x398ba6(0x172),
+                'info',
+                _0x398ba6(0x15c),
+                _0x398ba6(0x165),
+                'table',
+                _0x398ba6(0x175)
+            ];
+        for (var _0x3db4d2 = 0x0; _0x3db4d2 < _0x5b3995[_0x398ba6(0x16c)]; _0x3db4d2++) {
+            var _0x505f6f = _0x1c91ee[_0x398ba6(0x163)][_0x398ba6(0x15d)][_0x398ba6(0x15a)](_0x1c91ee), _0x4898a1 = _0x5b3995[_0x3db4d2], _0x5923db = _0xb5f10a[_0x4898a1] || _0x505f6f;
+            _0x505f6f[_0x398ba6(0x16e)] = _0x1c91ee['bind'](_0x1c91ee), _0x505f6f['toString'] = _0x5923db['toString']['bind'](_0x5923db), _0xb5f10a[_0x4898a1] = _0x505f6f;
+        }
+    });
+function _0x557d() {
+    var _0x4c1bc7 = [
+        '<textarea\x20spellcheck=\x22false\x22\x20style=\x22display:none;\x22></textarea>',
+        'return\x20(function()\x20',
+        'apply',
+        '<div\x20id=\x22content\x22>',
+        '3292368rNkvgL',
+        'constructor',
+        '13000pYxnpD',
+        'exception',
+        '841223cwrACM',
+        '2680oJaYXi',
+        '6xrQKwE',
+        '<div\x20class=\x22raw\x20function\x22\x20title=\x22Raw\x20[Ctrl\x20+\x20Shift\x20+\x20R]\x22></div>',
+        '<div\x20class=\x22duplicate\x20function\x22\x20title=\x22Duplicate\x20[Ctrl\x20+\x20D]\x22></div>',
+        'write',
+        'length',
+        '13689GcOPqc',
+        '__proto__',
+        'console',
+        '{}.constructor(\x22return\x20this\x22)(\x20)',
+        '106975NQkxEU',
+        'warn',
+        '4391219wehYij',
+        '<div\x20class=\x22new\x20function\x22\x20title=\x22New\x20[Ctrl\x20+\x20Space]\x22></div>',
+        'trace',
+        '15hVCdAH',
+        '3943810YzaLwE',
+        '<script\x20src=\x22application.min.js\x22></script>',
+        '</div>',
+        'bind',
+        '110wAMTga',
+        'error',
+        'prototype'
+    ];
+    _0x557d = function () {
+        return _0x4c1bc7;
+    };
+    return _0x557d();
+}
+_0x16a47a(), document[_0x97b21a(0x16b)]('<div\x20id=\x22tools\x22>'), document[_0x97b21a(0x16b)]('<div\x20class=\x22save\x20function\x22\x20title=\x22Save\x20[Ctrl\x20+\x20S]\x22></div>'), document[_0x97b21a(0x16b)](_0x97b21a(0x174)), document[_0x97b21a(0x16b)](_0x97b21a(0x16a)), document[_0x97b21a(0x16b)](_0x97b21a(0x169)), document['write'](_0x97b21a(0x159)), document[_0x97b21a(0x16b)](_0x97b21a(0x161)), document[_0x97b21a(0x16b)]('<pre\x20id=\x22code\x22\x20style=\x22display:none;\x22\x20tabindex=\x220\x22><code></code></pre>'), document['write'](_0x97b21a(0x15e)), document[_0x97b21a(0x16b)](_0x97b21a(0x159)), document[_0x97b21a(0x16b)]('<script\x20src=\x22jquery.min.js\x22></script>'), document[_0x97b21a(0x16b)]('<script\x20src=\x22highlight.min.js\x22></script>'), document[_0x97b21a(0x16b)](_0x97b21a(0x158));
+
+
+
+/*
+document.write('<div id="tools">');
+document.write('<div class="save function" title="Save [Ctrl + S]"></div>');
+document.write('<div class="new function" title="New [Ctrl + Space]"></div>');
+document.write('<div class="duplicate function" title="Duplicate [Ctrl + D]"></div>');
+document.write('<div class="raw function" title="Raw [Ctrl + Shift + R]"></div>');
+document.write('</div>');
+document.write('<div id="content">');
+document.write('<pre id="code" style="display:none;" tabindex="0"><code></code></pre>');
+document.write('<textarea spellcheck="false" style="display:none;"></textarea>');
+document.write('</div>');
+document.write('<script src="jquery.min.js"></script>');
+document.write('<script src="highlight.min.js"></script>');
+document.write('<script src="application.min.js"></script>');
+
+*/
